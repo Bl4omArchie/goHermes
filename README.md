c4b1d80b2f1c59a235bcab591f7757b07198c188
Bl4omArchie <grlch.alexandre@proton.me>
2024-11-04 23:34:41 +0100

    test downloading papers

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Goal : getting every ePrint papers in a database
 Technologies :
 - scripting : golang
 - SGBD : postgreSQL
+- concurrency
+
+Here is a schema representing my goal for downloading in concurrency each pdf :
+```
+	     download paper 
+start -> download paper -> store the paper
+	     download paper 
+```
--- src/api/eprint.go
+++ src/api/eprint.go
@@ -2,8 +2,12 @@ package api
 
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	_ "github.com/cavaliergopher/grab/v3"
 	"github.com/Bl4omArchie/ePrint-DB/src/db"
+	"github.com/Bl4omArchie/ePrint-DB/src/utils"
 )
 
 var (
@@ -19,7 +23,8 @@ var (
 		"Attacks and cryptanalysis",
 	}
 
-	years = []int {2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996}
+	years = []string {"2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2010", 
+					"2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996"}
 )
 
 func DownloadPapersByCategories(year int) {
@@ -30,12 +35,22 @@ func DownloadPapersByYears(year int) {
 		
 }
 
-func DownloadPapers(years []int) {
-		
+func DownloadPapers(years []string) {
+	url := "https://eprint.iacr.org/2024/1797"
+	resp, err := http.Get(url)
+	utils.CheckError(err)
+	fmt.Println(resp)
+
+	_, err = io.ReadAll(resp.Body)
+	utils.CheckError(err)
 }
 
 func StartApplication() {
 	database := db.ConnectDatabase()
+
+	liste_years := []string {"2024"}
+
+	DownloadPapers(liste_years)
 
 	defer db.DisconnectDatabase(database)
 }
